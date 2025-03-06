0856bdc67532ccaa6adeb25f214660cf8c50954c
sunggyu-jeong <jskoder0109@gmail.com>
2025-03-06 13:41:48 +0900

    feat. 라이브러리 추가, 배치 스케줄링 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-logging")
 	implementation("org.apache.poi:poi-ooxml:5.2.3")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -29,6 +30,11 @@ dependencies {
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
 	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+	implementation("org.slf4j:slf4j-api:2.0.7")
+	implementation("ch.qos.logback:logback-classic:1.5.16")
+	implementation("ch.qos.logback:logback-core:1.5.16")
+	implementation("org.json:json:20250107")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/sunggyu/news_scraper_batch/batch/scheduler/BatchScheduler.kt
+++ src/main/kotlin/com/sunggyu/news_scraper_batch/batch/scheduler/BatchScheduler.kt
@@ -1,19 +1,68 @@
 package com.sunggyu.news_scraper_batch.batch.scheduler
 
-import com.sunggyu.news_scraper_batch.batch.service.ApiService
+import com.sunggyu.news_scraper_batch.batch.service.AppApiService
 import com.sunggyu.news_scraper_batch.batch.service.EmailService
 import com.sunggyu.news_scraper_batch.batch.service.ExcelService
+import kotlinx.coroutines.runBlocking
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Component
 class BatchScheduler(
-    private val apiService: ApiService,
+    private val appApiService: AppApiService,
     private val excelService: ExcelService,
     private val emailService: EmailService
 ) {
-    @Scheduled(cron = "0 0 9 ? * MON-FRI")
-    fun runBatchJob() {
+    private val logger: Logger = LoggerFactory.getLogger(BatchScheduler::class.java)
 
+    @Scheduled(cron = "0 */5 * * * ?")
+    fun runBatchJob() = runBlocking {
+        execute()
+    }
+
+//    @Scheduled(cron = "0 0 9 ? * MON-FRI")
+    suspend fun execute() {
+        try {
+            // 1. 로그인 요청으로 엑세스 토큰 설정
+            val loginResponse = appApiService.requestLogin();
+            logger.debug(">>>>>>>>>>>>>> 로그인 응답값 {}", loginResponse)
+            // 2. 사용자의 키워도 믁록 조회
+            val keywordResponse = appApiService.getKeywordList()
+            logger.debug(">>>>>>>>>>>>>> 검색어 응답값 {}", keywordResponse)
+            val queries = keywordResponse.data.map { el -> el.keyword }.joinToString(",")
+            // 3. 검색어, 검색일을 기준으로 뉴스정보 조회
+            // 검색일: 조회일 기준 어제날짜 ~ 당일
+            val startDate = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+            val endDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+            val newsList = appApiService.getNews(queries, startDate, endDate)
+            logger.debug(">>>>>>>>>>>>>> 뉴스 결과값 {}", newsList)
+
+            // 4. 검색된 정보로 엑셀파일 생성
+            val excelBytes = excelService.createExcelFile(newsList.data, startDate, endDate)
+
+            emailService.sendEmail(
+                to = "coder3306@goodsoft.io",
+                cc = null,
+                subject = "[${endDate}] 최신 뉴스 공유드립니다.",
+                text = """
+                    |안녕하세요,
+                    |${endDate}의 최신 뉴스를 정리하여 첨부해드립니다.
+                    |유용한 정보가 되길 바라며, 궁금한 사항이 있으시면 언제든지 문의 주세요.
+                    |
+                    |항상 건강하고 좋은 하루 보내세요!
+                    |
+                    |감사합니다.
+                """.trimMargin(),
+                attachmentFilename = "${endDate} news.xlsx",
+                attachmentData = excelBytes
+            )
+        } catch (e: Exception) {
+            logger.error(">>>>>>>>>>>>>> 배치 실행 중 {}", e.message)
+            throw Error(e);
+        }
     }
 }
