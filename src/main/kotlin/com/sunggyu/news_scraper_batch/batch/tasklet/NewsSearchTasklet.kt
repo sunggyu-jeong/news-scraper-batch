2b0212b61c391a56cfe6a136ef1c140db7f4334c
sunggyu-jeong <jskoder0109@gmail.com>
2025-03-10 17:14:51 +0900

    feat. 현재 날짜가 월요일일 경우, 3일 전 ~ 오늘날짜까지의 뉴스 정보 조회

--- src/main/kotlin/com/sunggyu/news_scraper_batch/batch/tasklet/NewsSearchTasklet.kt
+++ src/main/kotlin/com/sunggyu/news_scraper_batch/batch/tasklet/NewsSearchTasklet.kt
@@ -3,28 +3,30 @@ package com.sunggyu.news_scraper_batch.batch.tasklet
 import com.google.gson.Gson
 import com.sunggyu.news_scraper_batch.batch.getPastDate
 import com.sunggyu.news_scraper_batch.batch.runWithRetry
-import com.sunggyu.news_scraper_batch.batch.service.AppApiService
+import com.sunggyu.news_scraper_batch.batch.service.impl.ApiServiceImpl
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.stereotype.Component
+import java.time.DayOfWeek
 import java.time.LocalDate
 
 @Component
 @StepScope
 class NewsSearchTasklet(
-    private val appApiService: AppApiService
+    private val apiServiceImpl: ApiServiceImpl
 ): Tasklet {
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
         val jobExecutionContext = chunkContext.stepContext.stepExecution.jobExecution.executionContext
 
         runWithRetry(contribution.stepExecution) {
             val queries = jobExecutionContext.get("queries") as String
-            val startDate = getPastDate()
+            val today = LocalDate.now().dayOfWeek
+            val startDate = if (today == DayOfWeek.MONDAY) getPastDate(3) else getPastDate()
             val endDate = getPastDate(0)
-            val newsList = appApiService
+            val newsList = apiServiceImpl
                             .getNews(queries, startDate, endDate)
                             .execute()
                             .body()
