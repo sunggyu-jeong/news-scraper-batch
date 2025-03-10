1262344ecc80caff4356cd479a74a5328277e83f
sunggyu-jeong <jskoder0109@gmail.com>
2025-03-10 17:15:10 +0900

    chore. api 명칭 변경

--- src/main/kotlin/com/sunggyu/news_scraper_batch/batch/service/impl/ApiServiceImpl.kt
+++ src/main/kotlin/com/sunggyu/news_scraper_batch/batch/service/impl/ApiServiceImpl.kt
@@ -1,10 +1,11 @@
-package com.sunggyu.news_scraper_batch.batch.service
+package com.sunggyu.news_scraper_batch.batch.service.impl
 
 import com.sunggyu.news_scraper_batch.batch.domain.common.ApiResponse
 import com.sunggyu.news_scraper_batch.batch.domain.request.LoginRequest
 import com.sunggyu.news_scraper_batch.batch.domain.response.KeywordResponse
 import com.sunggyu.news_scraper_batch.batch.domain.response.LoginResponse
 import com.sunggyu.news_scraper_batch.batch.domain.response.NewsResponse
+import com.sunggyu.news_scraper_batch.batch.service.ApiService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service
 import retrofit2.Call
 
 @Service
-class AppApiService(
+class ApiServiceImpl(
     private val apiService: ApiService,
     @Value("\${username}") private val username: String,
     @Value("\${password}") private val password: String
@@ -21,7 +22,12 @@ class AppApiService(
 
     fun getNews(queries: String,startDate: String, endDate: String): Call<ApiResponse<List<NewsResponse>>> {
         try {
-            return apiService.getNewsList(queries, startDate, endDate)
+            val request = mapOf(
+                "queries" to queries,
+                "startDate" to startDate,
+                "endDate" to endDate
+            )
+            return apiService.getNewsList(request)
         } catch (e: Exception) {
             logger.error("뉴스 데이터 조회 중 오류발생: {}", e.message, e)
             throw Exception("뉴스 데이터 조회 중 오류발생: ${e.message}")
