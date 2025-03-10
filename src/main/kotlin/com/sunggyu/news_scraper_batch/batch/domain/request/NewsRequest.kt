b5ab059721db04b1975547981e196cccc597f51e
sunggyu-jeong <jskoder0109@gmail.com>
2025-03-10 17:10:06 +0900

    chore. 데이터 클래스 정렬방법 변경

--- src/main/kotlin/com/sunggyu/news_scraper_batch/batch/domain/request/NewsRequest.kt
+++ src/main/kotlin/com/sunggyu/news_scraper_batch/batch/domain/request/NewsRequest.kt
@@ -1,3 +1,7 @@
 package com.sunggyu.news_scraper_batch.batch.domain.request
 
-data class NewsRequest(val queries: String, val startDate: String, val endDate: String)
+data class NewsRequest (
+    val queries: String,
+    val startDate: String,
+    val endDate: String
+)
